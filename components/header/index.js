29a8b3ffca371596527e661f98a0ee6520699b53
mohan <patidarmohan0907@gmail.com>
2023-08-02 20:38:51 +0530

    create a search box and cart

--- components/header/index.js
+++ components/header/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import styles from './styles.module.scss';
 import Ad from "./Ad";
 import Top from "./Top";
+import Main from './Main';
 export default function Header() {
   return (
     <header className={styles.header}>
       <Ad />
-      <Top/>
+      <Top />
+      <Main/>
     </header>
   )
 }
